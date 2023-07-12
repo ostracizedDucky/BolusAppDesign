6d18a0be40e85ef40558a7a74fd1299e2e028b48
Abe Park <abepark013@gmail.com>
2023-07-11 21:33:15 -0400

    Initial Commit

---
+++ BolusAppDesign/BolusAppDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  BolusAppDesignApp.swift
+//  BolusAppDesign
+//
+//  Created by Abe on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct BolusAppDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
